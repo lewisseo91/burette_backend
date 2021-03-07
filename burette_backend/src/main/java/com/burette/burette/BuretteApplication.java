fe278c8666376f6700ae7584458617b3acba06a4
lewisseo91 <togokr@naver.com>
2021-03-07 12:11:02 +0900

    [feat] 프로젝트 생성
     - 스프링 부트
     - apache 9.0

---
+++ burette_backend/src/main/java/com/burette/burette/BuretteApplication.java
@@ -0,0 +1,11 @@
+package com.burette.burette;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuretteApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BuretteApplication.class, args);
+    }
+}
