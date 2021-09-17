4627a6137e114df25c084d14516780be5793da7c
lewisseo91 <togokr@naver.com>
2021-09-17 19:21:29 +0900

    feat : 유저 리스트 받아오기 추가
     - 테스트 추가
     - TODO [성능] : Pageable 적용

--- buretteUser/src/main/java/com/takeon/burette/user/ui/UserController.java
+++ buretteUser/src/main/java/com/takeon/burette/user/ui/UserController.java
@@ -7,19 +7,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 // 맵핑은 컨트롤러 단위로
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
-//    private UserServiceOld userServiceOld;
-
     @Autowired
     private UserService userService;
-
-//    public UserController(UserServiceOld userServiceOld) {
-//        this.userServiceOld = userServiceOld;
-//    }
 
     @GetMapping("/test")
     public ResponseEntity Test() {
@@ -30,17 +26,13 @@ public class UserController {
     @PostMapping("/create")
     public ResponseEntity createUser(@RequestBody UserRequest userRequest) {
         // 등록
-        System.out.println("ldkfsajfdlksajdf");
-        System.out.println(userRequest.toString());
-//        int id = userServiceOld.registerUser(userRequest);
-//        System.out.println(id);
         User user = userService.save(userRequest);
+
         return ResponseEntity.ok().body(user);
     }
 
     @PostMapping("/login")
     public ResponseEntity loginUser(@RequestBody LoginRequest loginRequest) {
-//        String token = userServiceOld.loginUser(loginRequest);
         String token = userService.loginUser(loginRequest);
         if (token == null) {
             return ResponseEntity.badRequest().build();
@@ -51,19 +43,28 @@ public class UserController {
 
     @PostMapping("/writable")
     public ResponseEntity isWritable(@RequestBody TokenRequest tokenRequest) {
-//        boolean isWritable = userServiceOld.isWritable(tokenRequest);
         boolean isWritable = userService.isWritable(tokenRequest);
-        System.out.println("isWritable : " + isWritable);
+
         return isWritable ? ResponseEntity.ok().build() : ResponseEntity.status(401).build();
     }
 
     @GetMapping("/user/{id}")
     public ResponseEntity<UserResponse> getUserById(@PathVariable String userId) {
-//        UserResponse response = userServiceOld.getUserById(userId);
         UserResponse response = userService.getUserByUserId(userId);
         if (response == null) {
             return ResponseEntity.badRequest().build();
         }
+
+        return ResponseEntity.ok().body(response);
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<UserResponse>> getUsers() {
+        List<UserResponse> response = userService.getUsers();
+        if (response == null) {
+            return ResponseEntity.badRequest().build();
+        }
+
         return ResponseEntity.ok().body(response);
     }
 
